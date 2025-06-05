f53fa675691434e686d74f4ac425e86e4e75d1d2
Musharaf Ahmed <musharafahmed780@gmail.com>
2025-06-05 13:27:09 +0500

    initial code

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Initialize Express
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+// Test API
+app.get("/", (req, res) => {
+  res.send("API works fine.");
+});
+
+// Database Connection
+mongoose.set("strictQuery", false);
+
+const dbURI = `mongodb+srv://musharafahmed780:${process.env.MongoDBPassword}@cluster0.zpmq3cl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose
+  .connect(dbURI)
+  .then(() => console.log("Database is connected"))
+  .catch((err) => console.error("Database connection error:", err));
+
+// Start Server
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Server is running on Port # ${PORT}`);
+});
