3844529885851d5dc40d5c9ec26924eb439bf269
Musharaf Ahmed <musharafahmed780@gmail.com>
2025-06-05 15:52:22 +0500

    Optimized function

--- Controllers/AiChatController.js
+++ Controllers/AiChatController.js
@@ -8,105 +8,162 @@ const openai = new OpenAI({
 exports.basicChat = async (req, res) => {
   const { userId, userMessage, _id } = req.body;
 
-  let context = [];
+  const userInput = userMessage?.toString().trim();
+  if (!userInput) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "User message is required" });
+  }
 
-  let chat = [];
+  try {
+    let conversation = await ChatMode.findOne({ _id });
 
-  const existingConversation = await ChatMode.findOne({
-    _id: _id,
-  });
+    if (!conversation) {
+      conversation = new ChatMode({
+        userId,
+        context: [{ role: "system", content: "Act like a restaurant owner" }],
+        chat: [],
+      });
+    }
 
-  if (!existingConversation) {
-    context.push({
-      role: "system",
-      content: "Act like a restaurant owner",
+    conversation.context.push({ role: "user", content: userInput });
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: conversation.context,
     });
 
-    try {
-      const createChat = async () => {
-        const response = await openai.chat.completions.create({
-          model: "gpt-3.5-turbo",
-          messages: context,
-        });
+    const responseMessage = response.choices[0].message.content;
 
-        const responseMessage = response.choices[0].message.content;
-        chat.push({
-          user: req.body.userMessage,
-          ai: responseMessage,
-        });
+    conversation.chat.push({
+      user: userInput,
+      ai: responseMessage,
+    });
 
-        const _newConversation = new ChatMode({
-          userId: userId,
-          context: context,
-          chat: chat,
-        });
+    const savedConversation = await conversation.save();
 
-        const savedChat = await _newConversation.save();
-        return res.status(201).json({
-          status: "Success",
-          message: "Chat created successfully",
-          responseMessage: responseMessage,
-          data: savedChat,
-        });
-      };
+    const message =
+      _id && conversation._id.equals(_id)
+        ? "Chat updated successfully"
+        : "Chat created successfully";
 
-      const userInput = userMessage.toString().trim();
-      if (!userInput) return;
-
-      context.push({
-        role: "user",
-        content: userInput,
-      });
-
-      await createChat();
-    } catch (error) {
-      return res.status(500).json({
-        status: "error",
-        message: "Something went wrong",
-        error: error.message || error,
-      });
-    }
-  } else {
-    context = existingConversation.context;
-    chat = existingConversation.chat;
-
-    try {
-      const createChat = async () => {
-        const response = await openai.chat.completions.create({
-          model: "gpt-3.5-turbo",
-          messages: context,
-        });
-
-        const responseMessage = response.choices[0].message.content;
-        chat.push({
-          user: req.body.userMessage,
-          ai: responseMessage,
-        });
-
-        const data = await existingConversation.save();
-        return res.status(201).json({
-          status: "Success",
-          message: "Chat Update successfully",
-          responseMessage: responseMessage,
-          data: data,
-        });
-      };
-
-      const userInput = userMessage.toString().trim();
-      if (!userInput) return;
-
-      context.push({
-        role: "user",
-        content: userInput,
-      });
-
-      await createChat();
-    } catch (error) {
-      return res.status(500).json({
-        status: "error",
-        message: "Something went wrong",
-        error: error.message || error,
-      });
-    }
+    return res.status(201).json({
+      status: "Success",
+      message,
+      responseMessage,
+      data: savedConversation,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+      error: error.message || error,
+    });
   }
 };
+
+// exports.basicChat = async (req, res) => {
+//   const { userId, userMessage, _id } = req.body;
+
+//   let context = [];
+
+//   let chat = [];
+
+//   const existingConversation = await ChatMode.findOne({
+//     _id: _id,
+//   });
+
+//   if (!existingConversation) {
+//     context.push({
+//       role: "system",
+//       content: "Act like a restaurant owner",
+//     });
+
+//     try {
+//       const createChat = async () => {
+//         const response = await openai.chat.completions.create({
+//           model: "gpt-3.5-turbo",
+//           messages: context,
+//         });
+
+//         const responseMessage = response.choices[0].message.content;
+//         chat.push({
+//           user: req.body.userMessage,
+//           ai: responseMessage,
+//         });
+
+//         const _newConversation = new ChatMode({
+//           userId: userId,
+//           context: context,
+//           chat: chat,
+//         });
+
+//         const savedChat = await _newConversation.save();
+//         return res.status(201).json({
+//           status: "Success",
+//           message: "Chat created successfully",
+//           responseMessage: responseMessage,
+//           data: savedChat,
+//         });
+//       };
+
+//       const userInput = userMessage.toString().trim();
+//       if (!userInput) return;
+
+//       context.push({
+//         role: "user",
+//         content: userInput,
+//       });
+
+//       await createChat();
+//     } catch (error) {
+//       return res.status(500).json({
+//         status: "error",
+//         message: "Something went wrong",
+//         error: error.message || error,
+//       });
+//     }
+//   } else {
+//     context = existingConversation.context;
+//     chat = existingConversation.chat;
+
+//     try {
+//       const createChat = async () => {
+//         const response = await openai.chat.completions.create({
+//           model: "gpt-3.5-turbo",
+//           messages: context,
+//         });
+
+//         const responseMessage = response.choices[0].message.content;
+//         chat.push({
+//           user: req.body.userMessage,
+//           ai: responseMessage,
+//         });
+
+//         const data = await existingConversation.save();
+//         return res.status(201).json({
+//           status: "Success",
+//           message: "Chat Update successfully",
+//           responseMessage: responseMessage,
+//           data: data,
+//         });
+//       };
+
+//       const userInput = userMessage.toString().trim();
+//       if (!userInput) return;
+
+//       context.push({
+//         role: "user",
+//         content: userInput,
+//       });
+
+//       await createChat();
+//     } catch (error) {
+//       return res.status(500).json({
+//         status: "error",
+//         message: "Something went wrong",
+//         error: error.message || error,
+//       });
+//     }
+//   }
+// };
